4fee8872f408dfc6a92cf01c4534b9f8cb32195e
noeldevs17 <noeldrum17@hotmail.com>
2024-10-06 12:33:25 -0400

    -creacion de lazy loading de los modulos de auth y heroes

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+
+
 
 const routes: Routes = [
+{
+  path: '',
+  component: LayoutPageComponent
 
+}
 ]
 
 
--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-Page/layout-Page.component';
+import { NewPageComponent } from './pages/new-Page/new-Page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+    children: [
+      {path: 'new-hero', component: NewPageComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
